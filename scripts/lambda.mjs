6346f6fc34b1b4b3d42ff64876c0131e88c4a501
Dragan_Nikolic <kicaubuntu@gmail.com>
2025-12-30 14:34:22 +0100

    Update lambda.mjs

--- scripts/lambda.mjs
+++ scripts/lambda.mjs
@@ -1,1 +1,81 @@
+import {
+  S3Client,
+  HeadObjectCommand
+} from "@aws-sdk/client-s3";
 
+import mysql from 'mysql';
+
+const REGION = "us-east-1";
+
+const BUCKET = "dragan-nikolic-bucket1";
+
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+  TABLE_NAME,
+} = process.env;
+
+const s3 = new S3Client({
+  region: REGION
+});
+const dbConnection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: TABLE_NAME
+});
+
+export const handler = async (event, context) => {
+  console.log(" Dragan Event: ", event);
+  
+  let inconsistencies = [];
+
+  return new Promise((resolve, reject) => {
+      dbConnection.query("SELECT * FROM " + TABLE_NAME, async function(err, rows, fields) {
+          if (err) {
+              console.log('Error occured', err);
+              return reject(err);
+          }
+               
+          for (let i = 0; i < rows.length; i++) {
+              try {
+                  const data = await s3.send(new HeadObjectCommand({
+                      Bucket: BUCKET,
+                      Key: rows[i].name
+                  }));
+                  
+                  if (data.ContentLength != rows[i].size) {
+                      inconsistencies.push({
+                          'object': rows[i].name,
+                          'bucket': BUCKET,
+                          'Error': 'Image sizes dont match'
+                      });
+                  }
+              } catch (headErr) {
+                  if (headErr.name === 'NotFound') {
+                      console.log(`Inconsistency found for object: ${rows[i].name} in bucket: ${BUCKET}`);
+                      
+                      inconsistencies.push({
+                          'object': rows[i].name,
+                          'bucket': BUCKET,
+                          'Error': 'Object not found'
+                      });
+                  } else {
+                      console.log('Error occured', headErr);              
+                      return reject(headErr);
+                  }
+              }
+          }
+          
+
+          resolve({
+              isBase64Encoded:false,
+              headers: { },
+              statusCode: inconsistencies.length > 0 ? 400 : 200,
+              body: JSON.stringify(inconsistencies.length > 0 ? inconsistencies : "No inconsistencies found")
+          });
+      });
+  });
+};
